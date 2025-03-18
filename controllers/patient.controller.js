2e63585df24c053fa84378fe5d765c457c7e1078
Farhan22798 <mohdfarhan227@gmail.com>
2025-03-19 01:57:48 +0530

    again

--- controllers/patient.controller.js
+++ controllers/patient.controller.js
@@ -13,11 +13,13 @@ exports.getDoctorsForPatients = asyncHandler(async (req, res) => {
     res.json({ message: "doctor fetch success", result })
 })
 
+exports.getDocDetails = async (req, res) => {
+    const result = await Doctor.findById(req.params.did).select((" -createdAt -updatedAt -__v"))
+    res.json({ message: "doctor details fetch success", result })
+}
 
-exports.getDocDetails = asyncHandler(async (req, res) => {
-    const result = await Doctor.find({ doctor: req.params.did }).select((" -createdAt -updatedAt -__v"))
-    res.json({ message: "doctors details fetch success", result })
-})
+
+
 
 exports.bookAppointment = asyncHandler(async (req, res) => {
     const { doctor } = req.body
