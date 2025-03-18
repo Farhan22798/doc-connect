ebdd7a11a58831f476601a55b728096e16294cad
Farhan22798 <mohdfarhan227@gmail.com>
2025-03-19 03:32:41 +0530

    hosting

---
+++ dist/assets/Dashboard-CStFY6B7.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-Yz40dYcJ.js";const a=()=>r.jsx("div",{children:"Dashboard"});export{a as default};
--- index.js
+++ index.js
@@ -1,30 +1,35 @@
-const express = require("express")
-const mongoose = require("mongoose")
-require("dotenv").config()
-const cors = require("cors")
-const cookieParser = require("cookie-parser")
-const {  doctorProtected, adminProtected, patientProtected } = require("./midllewares/protected")
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const path = require("path"); 
 
+const { doctorProtected, adminProtected, patientProtected } = require("./midllewares/protected");
 
-app.use(express.json()) // req.body
-app.use(cookieParser()) // req.cookies
+const app = express();
+
+app.use(express.json()); 
+app.use(cookieParser()); 
 app.use(cors({
     origin: true,
-    credentials: true // cookie
-}))
+    credentials: true 
+}));
 
-app.use("/api/auth", require("./routes/auth.routes"))
-app.use("/api/doctor", doctorProtected, require("./routes/doctor.routes"))
-app.use("/api/admin", adminProtected, require("./routes/admin.routes"))
-app.use("/api/patient", patientProtected, require("./routes/patient.routes"))
-app.use("*", (req, res) => {
-     res.status(404).json({ message: "resource not found" })
-})
+app.use("/api/auth", require("./routes/auth.routes"));
+app.use("/api/doctor", doctorProtected, require("./routes/doctor.routes"));
+app.use("/api/admin", adminProtected, require("./routes/admin.routes"));
+app.use("/api/patient", patientProtected, require("./routes/patient.routes"));
 
+const frontendPath = path.join(__dirname, "dist"); // Point to dist folder
+app.use(express.static(frontendPath));
 
-mongoose.connect(process.env.MONGO_URL)
+app.get("*", (req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
+});
+
+mongoose.connect(process.env.MONGO_URL);
 mongoose.connection.once("open", () => {
-    console.log("db connected")
-    app.listen(process.env.PORT || 5000, console.log("server running"))
-})
+    console.log("DB Connected");
+    app.listen(process.env.PORT || 5000, console.log("Server Running"));
+});
