10a4510310f0921faa01a3271e88fff70f142768
Farhan22798 <mohdfarhan227@gmail.com>
2025-03-21 07:44:57 +0530

    back to zero

--- models/Appointments.js
+++ models/Appointments.js
@@ -1,31 +1,18 @@
-// const mongoose = require("mongoose")
-
-// const appointmentSchema = new mongoose.Schema({
-//     patient: { type: mongoose.Types.ObjectId, ref: "patient", required: true },
-//     doctor: { type: mongoose.Types.ObjectId, ref: "doctor", required: true },
-//     customerdetsreseaons: [
-//         {
-//             date: { type: String, required: true },
-//             time: { type: String, required: true },
-//             reason: { type: String, required: true },
-//         }
-//     ],
-//     status: { type: String, default: "Pending", enum: ["Pending","Cancelled", "Confirmed", "Old"] },
-
-
-// }, { timestamps: true })
-
-// module.exports = mongoose.model("appointment", appointmentSchema)
-
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
 const appointmentSchema = new mongoose.Schema({
-  patient: { type: mongoose.Schema.Types.ObjectId, ref: "patient", required: true },
-  doctor: { type: mongoose.Schema.Types.ObjectId, ref: "doctor", required: true },
-  date: { type: String, required: true },
-  time: { type: String, required: true },
-  reason: { type: String, required: true },
-  status: { type: String, default: "Pending", enum: ["Pending", "Cancelled", "Confirmed"] }
-}, { timestamps: true });
+    patient: { type: mongoose.Types.ObjectId, ref: "patient", required: true },
+    doctor: { type: mongoose.Types.ObjectId, ref: "doctor", required: true },
+    customerdetsreseaons: [
+        {
+            date: { type: String, required: true },
+            time: { type: String, required: true },
+            reason: { type: String, required: true },
+        }
+    ],
+    status: { type: String, default: "Pending", enum: ["Pending","Cancelled", "Confirmed", "Old"] },
 
-module.exports = mongoose.model("appointment", appointmentSchema);
+
+}, { timestamps: true })
+
+module.exports = mongoose.model("appointment", appointmentSchema)
--- routes/patient.routes.js
+++ routes/patient.routes.js
@@ -1,4 +1,4 @@
-const {  getDoctorsForPatients, getDocDetails, bookAppointment, getMyAppointments, getAppointmentHistory, getSearchedDoctors, getDoctorSlots } = require("../controllers/patient.controller")
+const {  getDoctorsForPatients, getDocDetails, bookAppointment, getMyAppointments, getAppointmentHistory, getSearchedDoctors } = require("../controllers/patient.controller")
 
 const router = require("express").Router()
 
@@ -7,8 +7,7 @@ router
 .get("/getDrForPatients",getDoctorsForPatients)
 .get("/getDrDetailsForPatients/:did",getDocDetails)
 .post("/book",bookAppointment)
-// .get("/fetch-appointments", getMyAppointments)
-.get("/slots/:doctorId/:date", getDoctorSlots)
+.get("/fetch-appointments", getMyAppointments)
 .get("/fetch-appointments-history", getAppointmentHistory)
 .get("/fetch-search", getSearchedDoctors)
 
